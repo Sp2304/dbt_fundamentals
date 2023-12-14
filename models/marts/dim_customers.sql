6ac41543711e37daf47562298767c7c196252954
Vriddhachalam S <vriddhachalam.shanmugam@anjanetech.com>
2023-12-14 10:33:23 +0000

    new branch

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -67,3 +67,15 @@ final as (
 )
 
 select * from final
+
+➕
+customer_orders.value
+            {% for payment_method in payment_methods -%}
+
+         sum(case when payment_method = '{{ payment_method }}' then amount_usd else 0 end) as {{ payment_method }}_amount        
+       
+       {%- if not loop.last -%}
+         ,
+       {% endif -%}
+
+       {%- endfor %}
